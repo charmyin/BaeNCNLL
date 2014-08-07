723abde024d7a94fd282175b1b7670b9a237ea49
charmyin <976582265@qq.com>
2014-08-07 23:58:48 +0800

    OK, imigarate from bae to github

--- bin/www.js
+++ bin/www.js
@@ -1,8 +1,8 @@
 #!/usr/bin/env node
 var debug = require('debug')('ncnll');
-var app = require('../app');
+var app = require('../server');
 
-app.set('port', process.env.PORT || 18080);
+app.set('port', process.env.PORT || 18088);
 
 var server = app.listen(app.get('port'), function() {
   debug('Express server listening on port ' + server.address().port);
